cb7f8c25a75a67091d612de08cee3867aeb924ea
tko <tko@tko-macbookpro.roam.corp.google.com>
2014-09-04 15:35:33 -0700

    Initial Commit

---
+++ scrollLoadingTests/scrollLoadingTests.m
@@ -0,0 +1,34 @@
+//
+//  scrollLoadingTests.m
+//  scrollLoadingTests
+//
+//  Created by Taeho Ko on 9/4/14.
+//  Copyright (c) 2014 google. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface scrollLoadingTests : XCTestCase
+
+@end
+
+@implementation scrollLoadingTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
